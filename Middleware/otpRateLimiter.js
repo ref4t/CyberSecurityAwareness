de793960daf7f2cf8082e3dd156c1dd7eef5057f
refat <md.abdullah.all.muktadir@gmail.com>
2025-06-11 00:53:24 +1000

    otpRateLimiter updated to 50

--- Middleware/otpRateLimiter.js
+++ Middleware/otpRateLimiter.js
@@ -3,7 +3,7 @@ import rateLimit from 'express-rate-limit';
 
 const otpRateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // limit each IP to 5 OTP requests per windowMs
+  max: 50, // limit each IP to 50 OTP requests per windowMs
   message: {
     success: false,
     message: "Too many OTP requests. Please try again later.",
