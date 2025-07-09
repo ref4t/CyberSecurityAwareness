0a2b246f30c6491e6f61a5d2ac08967e4240888c
Abdullah All Muktadir <39688365+ref4t@users.noreply.github.com>
2025-07-10 04:39:03 +1000

    Update AuthController.js

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -125,8 +125,8 @@ export const login = async (req, res) => {
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "Production",
-      sameSite: process.env.NODE_ENV === "Production" ? "none" : "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -145,8 +145,8 @@ export const login = async (req, res) => {
 export const logout = async (req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "Production",
-    sameSite: process.env.NODE_ENV === "Production" ? "none" : "strict",
+    secure: true,
+    sameSite: "none",
   });
   return res.status(200).json({ success: true, message: "Logged out" });
 };
